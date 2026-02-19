5c3f2ed071703b52ddf3d81854910e615eff8a56
Daniel Martin <danielamartin314@gmail.com>
2026-02-18 21:30:09 -0800

    add structure

--- sequence_alignment.py
+++ sequence_alignment.py
@@ -1,3 +1,21 @@
+def read_costs() -> None:
+
+    #TODO Implement a data structure to keep track of costs from input file
+
+
+
+
+    return
+
+
+
+def sadp() -> None:
+
+    #TODO Implement a dynamic programming solution to get the two aligned strings and the minimum cost
+
+
+
+
 def main() -> None:
 
     print("hello daniel")
