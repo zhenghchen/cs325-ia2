b694d837aff95e524c0de5e6809e1ba51d0eebfc
Daniel Martin <danielamartin314@gmail.com>
2026-02-18 23:08:12 -0800

    base sadp

--- sequence_alignment.py
+++ sequence_alignment.py
@@ -9,11 +9,25 @@ def read_costs() -> None:
 
 
 
-def sadp() -> None:
+def sadp(costs: dict, string1: str, n: int, string2: str, m: int) -> int:
 
     #TODO Implement a dynamic programming solution to get the two aligned strings and the minimum cost
 
+    s: list[list[int]] = []
 
+    for i in range(n):
+        for j in range(m):
+            # Base case
+            if i == 0 or j == 0:
+                s[i][j] = costs[string1[i]][string2[j]]
+
+
+            s[i][j] = min(s[i-1][j-1] + costs[string1[i]][string2[j]], s[i-1][j] + costs['-'][string2[j]], s[i][j-1] + costs[string1[i]]['-'])
+
+
+
+    min_num = s[n][m]
+    return min_num
 
 
 def main() -> None:
