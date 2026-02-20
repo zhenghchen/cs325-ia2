ec0d7a5c0134d8cc97961d25d62696a51c2d75b0
Daniel Martin <danielamartin314@gmail.com>
2026-02-19 18:43:49 -0800

    finished fileio

--- sequence_alignment.py
+++ sequence_alignment.py
@@ -1,3 +1,5 @@
+import sys
+
 def read_costs(cost_path) -> dict[str, dict[str, int]]:
     # reads the cost_path and puts it into a array of strings
     with open(cost_path, "r") as f:
@@ -31,10 +33,25 @@ def read_costs(cost_path) -> dict[str, dict[str, int]]:
     return cost
 
 
+def read_input(filename: str) -> list[tuple[str, str]]:
+    pairs = []
+    with open(filename, 'r') as f:
+        for line in f:
+            line = line.strip()
+            if line:
+                left, right = line.split(",")
+                pairs.append((left.strip(), right.strip()))
 
-def sadp(costs: dict, string1: str, string2: str) -> int:
+    return pairs
+
+
+
+def sadp(costs: dict, string1: str, string2: str) -> tuple[str, str, int]:
 
     #TODO Implement a dynamic programming solution to get the two aligned strings and the minimum cost
+
+    seq1: str = ""
+    seq2: str = ""
 
     n = len(string1)
     m = len(string2)
@@ -49,23 +66,71 @@ def sadp(costs: dict, string1: str, string2: str) -> int:
 
     for i in range(1, n+1):
         for j in range(1, m+1):
+
+            min_val = min(s[i-1][j-1] + costs[string1[i-1]][string2[j-1]], s[i-1][j] + costs[string1[i-1]]['-'], s[i][j-1] + costs['-'][string2[j-1]])
             
-            s[i][j] = min(s[i-1][j-1] + costs[string1[i-1]][string2[j-1]], s[i-1][j] + costs[string1[i-1]]['-'], s[i][j-1] + costs['-'][string2[j-1]])
+            s[i][j] = min_val
 
 
 
+    
+    #Backward pass
+    i = n
+    j = m
+
+    while i > 0 or j > 0:
+
+        if i == 0:
+            seq1 += '-'
+            seq2 += string2[j-1]
+            j -= 1
+        elif j == 0:
+            seq1 += string1[i-1]
+            seq2 += '-'
+            i -= 1
+        elif s[i][j] == s[i-1][j-1] + costs[string1[i-1]][string2[j-1]]:
+            seq1 += string1[i-1]
+            seq2 += string2[j-1]
+            i -= 1
+            j -= 1
+        elif s[i][j] == s[i-1][j] + costs[string1[i-1]]['-']:
+            seq1 += string1[i-1]
+            seq2 += '-'
+            i -= 1
+        else:
+            seq1 += '-'
+            seq2 += string2[j-1]
+            j -= 1
+
+        
+    seq1 = seq1[::-1]
+    seq2 = seq2[::-1]
     min_num = s[n][m]
-    return min_num
+    return seq1, seq2, min_num
+
+
+def output(filename: str, lines: list[tuple[str, str, int]]) -> None:
+
+    with open(filename, "w") as f:
+        for line in lines:
+            f.write(line[0] + "," + line[1] + ":" + str(line[2]) + "\n")
+
+    return
+
 
 
 def main() -> None:
 
     costs = read_costs("imp2cost.txt")
-    str1 = "AAATGTGTGTGTTCCCCAACGATGTCTCTAGAAGACGAACATCCC"
-    str2 = "ATGGAAACGTGAACCTAACTAACACATATGGATCCGACTGACGTTCTCTGATGTAGCCT"
-    min_cost = sadp(costs, str1, str2)
+    inputs = read_input(sys.argv[1])
 
-    print(min_cost)
+    tuplist = []
+    for input in inputs:
+        tup = sadp(costs, input[0], input[1])
+        tuplist.append(tup)
+    
+
+    output("output.txt", tuplist)
     
 if __name__ == "__main__":
     main()
